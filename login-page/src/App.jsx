04566e60e2d8db44995274085046e13b4381f569
NadeeraPereraD <98936438+NadeeraPereraD@users.noreply.github.com>
2025-11-26 11:50:20 +0530

    Created new vite project and config tailwind css

---
+++ login-page/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='bg-red-900'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptates velit deleniti perspiciatis dolores et odit consectetur, dolor sunt eum, fugiat corporis ex quisquam animi perferendis culpa voluptas eius excepturi.</p>
+    </>
+  )
+}
+
+export default App
