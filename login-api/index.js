691598892e618737c54196ba167a19f09e35815e
NadeeraPereraD <98936438+NadeeraPereraD@users.noreply.github.com>
2025-11-27 14:04:52 +0530

    Connected with DB

--- login-api/index.js
+++ login-api/index.js
@@ -1,2 +1,17 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
+
+const app = express();
+
+let mongoUrl = process.env.MONGO_URL;
+
+mongoose.connect(mongoUrl);
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+    console.log("MongoDB connection established successfully");
+});
