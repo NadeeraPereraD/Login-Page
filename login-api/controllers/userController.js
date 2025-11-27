c480ab2756a093796dee1041176034e21f854984
NadeeraPereraD <98936438+NadeeraPereraD@users.noreply.github.com>
2025-11-27 16:34:34 +0530

    Added Login API end point

--- login-api/controllers/userController.js
+++ login-api/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/users.js";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -20,3 +21,41 @@ export async function registerUser(req, res) {
     res.status(500).json({ error: "User registration failed" });
   }
 }
+
+export const loginUser = async (req, res) => {
+  try {
+    const data = req.body;
+
+    const user = await User.findOne({ email: data.email });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(data.password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ error: "Login failed: Incorrect password" });
+    }
+
+    const token = jwt.sign(
+      {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        id: user._id, 
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" } 
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user,
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
